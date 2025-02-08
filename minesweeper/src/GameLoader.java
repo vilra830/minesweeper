940faa4c8d603f903d47de639a86c57c713ec9c5
vilra830 <villafuerte.rhea@gmail.com>
2025-02-08 20:41:24 +1100

    creates GameLoader that initializes game

---
+++ minesweeper/src/GameLoader.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class GameLoader {
+
+ 
+    public void loadGame(){
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Display name of game
+        System.out.println("MINESWEEPER GAME");
+        System.out.println("Please type 'start' to begin game.");
+
+
+        String input = scanner.next();
+
+        if(input.equalsIgnoreCase("start")) {
+
+            System.out.println("Starting the game...");
+
+            startGame();
+
+        } else {
+
+            System.out.println("Invalid input. Please type 'start' to begin game");
+        }
+
+        scanner.close();
+
+    }
+
+    public void startGame(){
+
+        System.out.println("This will call another class that creates the grid");
+        System.out.println("Game under construction");
+    }
+    
+}
---
+++ minesweeper/src/Minesweeper.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Minesweeper {
+    public static void main(String[] args) {
+
+
+        GameLoader gameLoader = new GameLoader();
+
+        gameLoader.loadGame();
+    }
+    
+}
